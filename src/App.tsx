0b59121818c19917bcfaf16983392866bffb3ba7
YangKai232323 <rodontop1@gmail.com>
2023-09-19 14:45:07 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+function App() {
+
+  return (
+        <div className="flex flex-row w-screen h-screen bg-black justify-between items-center gap-16">
+          <div className="bg-slate-600">Sideboard</div>
+          <div className="bg-orange-600">Main</div>
+          <div className="bg-green-700">Inventory</div>
+        </div>
+  )
+}
+
+export default App
